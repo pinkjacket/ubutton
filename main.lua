1879416d774723147c7a65e05f8b960b4a2cae1b
James Donlan <jacketlizard@live.com>
2020-01-13 01:55:06 -0800

    game stops when timer runs out, replaying resets score

--- main.lua
+++ main.lua
@@ -6,22 +6,30 @@ function love.load()
 
   score = 0
   timer = 10
+  gameState = 1
 
   myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
-  if timer > 0 then
-    timer = timer - dt
-  end
-  if timer < 0 then
-    timer = 0
+  if gameState == 2 then
+    if timer > 0 then
+      timer = timer - dt
+    end
+
+    if timer < 0 then
+      timer = 0
+      gameState = 1
+      score = 0
+    end
   end
 end
 
 function love.draw()
-  love.graphics.setColor(0, 1, 0)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    love.graphics.setColor(0, 1, 0)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
   love.graphics.setFont(myFont)
   love.graphics.setColor(1, 1, 1)
   love.graphics.print(score)
@@ -29,12 +37,17 @@ function love.draw()
 end
 
 function love.mousepressed(x, y, click, istouch)
-  if click == 1 then
+  if click == 1 and gameState == 2 then
     if getDistance(button.x, button.y, love.mouse.getX(), love.mouse.getY()) < button.size then
       score = score + 1
       button.x = math.random(button.size, love.graphics.getWidth() - button.size)
       button.y = math.random(button.size, love.graphics.getHeight() - button.size)
     end
+  end
+
+  if gameState == 1 then
+    gameState = 2
+    timer = 10
   end
 end
 
